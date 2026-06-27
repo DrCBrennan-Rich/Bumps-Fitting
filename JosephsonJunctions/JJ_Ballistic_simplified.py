c1001cf8a0d701af61e84d6cf84cda156c7ab919
Callum B-R <cbrphys@outlook.com>
2026-06-27 21:15:48 +0100

    Update JJ_Ballistic_simplified.py

--- JosephsonJunctions/JJ_Ballistic_simplified.py
+++ JosephsonJunctions/JJ_Ballistic_simplified.py
@@ -11,80 +11,72 @@ import matplotlib.pyplot as plt
 
 k_B = 8.617333262E-5 #eV/K
 SC_gap = 1.5E-3 #eV
-T = 4.2 #K
-xi = 0.3 #nm
-R = 1.4E-3 #ohms
-#Coherence lengths
-CoherenceLength_F1=0.3 #nm
-CoherenceLength_F2=0.16 #nm
+Temperature = 4.2 #K
+CoherenceLength = 0.3 #nm
+Resistance = 1.4E-3 #ohms
 
 def JC_model(d_F, Temperature, SC_gap, CoherenceLength, Resistance):
+#the np.sinc function is normalised as default so need to divide argument by pi
+    SincTerm = np.sinc(d_F/(np.pi*CoherenceLength)) 
     
-    SincTerm = np.sin(d_F/CoherenceLength)/(d_F/CoherenceLength)
-    
-    return 1E3*(np.pi*SC_gap*SC_gap)*np.abs(SincTerm)/(4*k_B*Temperature)
+    return (np.pi*SC_gap*SC_gap)*np.abs(SincTerm)/(4*k_B*Temperature*Resistance)
 
 #Load the data from the file Data.txt
-d_F,y,dy = np.loadtxt('L11 data 4.2K.txt').T 
+d,y,dy = np.loadtxt('L11 data 4.2K.txt').T #units of nm, mA, mA
 
 Model = bmp.Curve(
     JC_model,
-    d_F, y, dy,
-    Temperature=T,
+    d, y, dy,
+    Temperature=Temperature,
     SC_gap = SC_gap,
-    CoherenceLength=xi, 
-    Resistance=R)
+    CoherenceLength=CoherenceLength, 
+    Resistance=Resistance)
 
 ### Limits of fitting values ###
 
-#Model.Amplitude.range(20,120)
-#Model.CoherenceLength_F1.dev(std=0.1, mean=0.3, limits=None)
+Model.CoherenceLength.range(1,3)
+Model.SC_gap.range(1E-3,2E-3)
+Model.Temperature.range(3,5)
+#Model.Resistance.range(1.3E-3,1.5E-3)
 
-#Model.d_0pi.range(0.0,0.9*np.pi*CoherenceLength_F2) #Due to the periodicity of d_0pi, this will be the paramter range
-
-#Model.CoherenceLength.range(0.1,0.3)
-#Model.CoherenceLength.range(0.1,0.2)
-
-#Model.CoherenceLength_F1.dev(std=0.1, mean=0.3, limits=None)
-#Model.CoherenceLength_F2.dev(std=0.1, mean=0.16, limits=None)
+#Model.CoherenceLength.dev(std=0.1, mean=0.3, limits=None)
+#Model.SC_gap.dev(std=0.1, mean=0.3, limits=None)
+#Model.Temperature.dev(std=0.1, mean=0.16, limits=None)
+#Model.Resistance.dev(std=0.1, mean=0.16, limits=None)
 
 #######
 
 #Initial values
 
-#Model.Amplitude.value = 130
-#Model.d_0pi.value = 1
+Model.CoherenceLength.value = 0.3
+Model.SC_gap.value = 1.5E-3
+Model.Temperature.value = 4.2
+#Model.Resistance.value = 1.4E-3
 
-#Model.CoherenceLength.value = 0.1
-#Model.CoherenceLength.value = 0.16
-
-#problem = bmp.FitProblem(Model)
+problem = bmp.FitProblem(Model)
 
 #This line is not strictly required, but allows you to run this py file check the initial parameters.
-#problem.show()
+problem.show()
 
 #Run some test values to see how they affect the final plot
-
-
 plt.errorbar(
-    d_F, y, yerr=dy,
-    fmt='o',
+    d, y, yerr=dy,
+    fmt='H',
     capsize=3,
     label='Experimental data'
 )
 
-
-for CoherenceLenght_test in [0.5]:
+for CoherenceLenght_test in [0.1,0.5,1,3]:
     ytest = JC_model(
-        d_F,
-        Temperature=T,
+        d,
+        Temperature=Temperature,
         SC_gap = SC_gap,
         CoherenceLength=CoherenceLenght_test, 
-        Resistance=R, 
+        Resistance=Resistance, 
     )
-    plt.plot(d_F, ytest, label=f"CoherenceLength={CoherenceLenght_test}",color='red')
+    plt.plot(d, ytest, label=f"CoherenceLength={CoherenceLenght_test}")
 
 plt.legend()
 plt.yscale('log')
-plt.savefig("BallisticSimp.svg", format="svg")
+plt.savefig("BallisticSimplified.svg", format="svg")
 plt.show()
