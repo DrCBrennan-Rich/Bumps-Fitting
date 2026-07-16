232c339df52be94cfc77fd15cb3964e2ef38ebc9
Callum B-R <cbrphys@outlook.com>
2026-07-16 11:38:39 +0100

    Update JJ_DiffuseStrongExchange.py

--- JosephsonJunctions/JJ_DiffuseStrongExchange.py
+++ JosephsonJunctions/JJ_DiffuseStrongExchange.py
@@ -93,21 +93,6 @@ def Find_Theta_NF(d_N, Omega, xi_N, theta_NS, gamma_BSN, theta_S):
     
     return theta_NF
 
-def Find_Theta_NS(d_N, xi_N, gamma_NF, gamma_BSN, Omega, eta, Chi, theta_S):
-    #Impliments equation A8
-    U = 2*gamma_NF*gamma_BSN*Chi*np.sqrt(Omega+eta*(1-Chi*Chi))
-    V = (Omega*d_N*gamma_BSN/xi_N) + np.cos(theta_S)
-    
-    a = (V*V)/(np.sin(theta_S)*np.sin(theta_S)) + 1
-    b = 2*U*V/(np.sin(theta_S)*np.sin(theta_S)) 
-    c = (U*U)/(np.sin(theta_S)*np.sin(theta_S)) - 1
-    
-    coeffs = [a,b,c]
-
-    theta_NS = np.arcsin(np.roots(coeffs))
-    
-    return theta_NS
-
 def Find_Theta_NS_Initial(d_N, Omega, xi_N, gamma_BSN, theta_S):
     #If eta and gamma_NF = 0 then this function will find theta_NS from equation A8
     A = (np.real(Omega)*d_N*gamma_BSN)/(xi_N*np.sin(theta_S))
@@ -252,7 +237,7 @@ def JC_DiffuseExchange(d_F, Temperature, Resistivity_N, SpinScatterTime,
          
         Chi2 = Solution[0] + 1j*Solution[1]
         
-        Term = np.real(gamma*np.exp(-gamma*d_F)*Chi1*Chi2)
+        Term = np.real(gamma*np.exp(-gamma*d_F)*Chi1*Chi1)
         J_c += Term
            
     return Amplitude*np.abs(J_c)
@@ -263,6 +248,12 @@ d,y,dy = np.loadtxt('PtCoPt data 4.2K.txt').T #units of nm, mA, mA
 d = np.array([0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 1.0, 0.30000000000000004, 0.44999999999999996, 0.6000000000000001, 0.75, 0.8999999999999999, 1.0499999999999998, 1.2000000000000002, 1.35, 0.15000000000000002, 1.5, 1.6500000000000001, 1.7999999999999998])
 y = np.array([np.float64(25.485540677019983), np.float64(20.24388234661639), np.float64(9.229536446202395), np.float64(2.3917306615253358), np.float64(5.332381933490123), np.float64(6.262335355328344), np.float64(5.555634474421091), np.float64(7.14605149171436), np.float64(4.670040152958881), np.float64(4.466469736108299), np.float64(4.402734073344201), np.float64(2.8669437006283083), np.float64(2.198601946982659), np.float64(0.31666206152922616), np.float64(1.018208817810008), np.float64(6.094153299948496), np.float64(6.307979995277196), np.float64(5.485744923060322), np.float64(2.581500267199711), np.float64(0.3440655265061016), np.float64(1.3730251946938403), np.float64(0.951189258094282), np.float64(0.08461134762633567), np.float64(38.13333333333333), np.float64(0.1905), np.float64(0.3), np.float64(0.12166666666666666)])
 dy = np.array([np.float64(1.1541353059558421), np.float64(1.3282852847429805), np.float64(0.43555339620836153), np.float64(0.183136722622837), np.float64(0.32397334928648797), np.float64(0.16637832288545412), np.float64(0.4313857996461637), np.float64(0.2067104216127845), np.float64(0.26935663136737165), np.float64(0.11061774240230109), np.float64(0.5189157313556868), np.float64(0.13401844281653402), np.float64(0.1469797151953465), np.float64(0.051007687876081016), np.float64(0.08432750457227471), np.float64(0.12104294939270999), np.float64(0.2824216946228165), np.float64(0.19224653742922326), np.float64(0.03978367734086671), np.float64(0.018540196895687144), np.float64(0.20333691412042817), 0.0053777623606668535, 0.004048255991005446, np.float64(1.7975291683617007), np.float64(0.0035000000000000027), np.float64(0.04999999999999999), np.float64(0.010137937550497038)])
+
+OrderingIndex = np.argsort(d)
+d = d[OrderingIndex]
+y = y[OrderingIndex]
+dy = dy[OrderingIndex]
+
 y = y/1.9E-3
 dy = dy/1.9E-3
 
@@ -285,8 +276,8 @@ Model = bmp.Curve(
 #Model.H.range(1E-5,3E-3)
 #Model.Temperature.range(1,10)
 Model.SpinScatterTime.range(1E-18,5E-11)
-Model.gamma_NF.range(1E-2,50)
-Model.Area.range(5E6,1E14)
+Model.gamma_NF.range(1E-4,10)
+Model.Area.range(5E6,1E16)
 
 #Model.CoherenceLength.dev(std=0.1, mean=0.3, limits=None)
 #Model.SC_gap.dev(std=0.1, mean=0.3, limits=None)
