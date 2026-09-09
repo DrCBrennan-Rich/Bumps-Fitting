03f83c35ec3b2ff5d30180354a26d616123fa922
Callum B-R <cbrphys@outlook.com>
2026-09-09 22:37:44 +0100

    Update JJ_Kuprianov.py

--- JosephsonJunctions/JJ_Kuprianov.py
+++ JosephsonJunctions/JJ_Kuprianov.py
@@ -14,7 +14,6 @@ plt.rcParams.update({'font.size': 40})
 k_B = constants.physical_constants['Boltzmann constant in eV/K'][0] #eV/K
 JunctionResistance = 1.55E-3 #Ohms
 
-#Coherence lengths
 #Amplitude = 260.452 #Current amplitude
 CriticalTemperature = 8.87267 #K
 CoherenceLength = 0.36178 #nm
