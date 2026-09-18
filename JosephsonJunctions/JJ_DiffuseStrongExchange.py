c73b07e6368e6bc1d9438df7efb70efc1acf1616
Callum B-R <cbrphys@outlook.com>
2026-09-18 21:06:15 +0100

    Update JJ_DiffuseStrongExchange.py

--- JosephsonJunctions/JJ_DiffuseStrongExchange.py
+++ JosephsonJunctions/JJ_DiffuseStrongExchange.py
@@ -452,6 +452,7 @@ def Find_SNF_Boundary_Chi(gamma_BNF, Omega, theta_NF_initial, theta_NS_initial,
         gamma_NF (float): Suppresion parameter between the normal metal
             and the superconductor (unitless).
         StepNumber (int): Number of steps performed between 0 and eta.
+        d_N (float): Thickness of the normal metal (nm).
         
 
     Returns:
