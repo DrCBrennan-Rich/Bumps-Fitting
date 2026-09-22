7aa896abc7cdf54f9da7fa673d8a5377d443f699
Callum B-R <cbrphys@outlook.com>
2026-09-22 23:42:53 +0100

    Update BumpsLinear.py

--- BumpsLinear.py
+++ BumpsLinear.py
@@ -10,28 +10,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def Line(x, Gradient, Intercept):
-    """Calculate the critical voltage across the Josephson junction according
-    to a ballistic model.
-
-    This function calculates the critical voltage, IcRn, as a function of 
-    ferromagnetic thickness of the weak link as presented in the Eq. 1 of 
-    the paper by Birge and Satchell: https://doi.org/10.1063/5.0195229.
+    """Calculate a linear line from the gradient and intercept.
 
     Args:
-        d_F (numpy.ndarray): List of (float) thicknesses of the ferromagnetic 
-            junction (nm).
-        CoherenceLength (float): Coherence length in the ferromagnet (nm).
-        SC_gap (float): Superconducting gap (eV).
-        PhiIncriment (int): Number of sub divisions of the 2*pi phase to be 
-            tested to find the maxium current (unitless).
-
+        x (numpy.ndarray): List of (float) x coordinates.
+        Gradient (float): Gradient of line.
+        Intercept (float): Intercept of line.
+        
     Returns:
-        IcRn (float): Voltage across the Josephson junction (uV).
+        y (float): y coordinate of line.
 
     Notes:
         Equation being solved is IcRn = pi*SC_gap^2*Sinc[d_F/CoherenceLength]/(4*T)
     """
-    return Gradient*x + Intercept
+    y = Gradient*x + Intercept
+    return y
 
 #Load the data from the file Data.txt
 x,y,dy = np.loadtxt('Data.txt').T
